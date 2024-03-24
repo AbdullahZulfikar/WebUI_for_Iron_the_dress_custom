5f91faa9dfd70df72e41460deb0b1c14d724ac21
Abdullah-Zulfiqar <abdullahzulfiqar123apple@gmail.com>
2024-03-24 21:48:32 +0500

    Creating header and created two components

--- ironthedress/src/app/page.tsx
+++ ironthedress/src/app/page.tsx
@@ -1,9 +1,10 @@
-
+import Header from "@/components/header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex flex-col min-h-screen ">
+      <Header/>
+      <div className="font-mono ">
         <h1> welcome to Iron The dress ui</h1>
       </div>
     </main>
--- ironthedress/src/components/header.tsx
+++ ironthedress/src/components/header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='flex justify-between px-10 py-5 font-serif text-gray-200'>
+        <h1 className='text-4xl'>IronTheDress</h1>
+        <div className="flex space-x-4 justify-center items-center">
+            <Link href="#">Home</Link>
+            <Link href="#">IronSpace</Link>
+        </div>
     </div>
   )
 }
