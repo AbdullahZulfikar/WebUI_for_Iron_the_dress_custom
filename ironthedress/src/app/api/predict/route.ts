f8941d2b5a6ecb34e5e1628f281f8fe74b1795e1
Shivansh Yadav <yadavshivansh@gmail.com>
2024-03-26 10:19:38 +0530

    add replicate api endpoint

---
+++ ironthedress/src/app/api/predict/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse, NextRequest } from "next/server";
+import Replicate from "replicate";
+
+export async function POST(req: NextRequest) {
+  if (req.method === 'POST') {
+    const data = await req.json();
+    const replicate = new Replicate({
+      auth: "your-api-key",
+    });
+    const output = await replicate.run(
+      "shivansh-yadav13/iron-the-dress:33757df23f3c36414a02ae99701f1c2dd1d4c3eddd29b5ae04fa0ed145044441",
+      {
+        input: {
+          input_image_base64: data.input_image,
+          mask_base64: data.mask_image,
+        }
+      }
+    );
+    console.log(output);
+    return new NextResponse(JSON.stringify({ output_base64: output }));
+  } else {
+    return new NextResponse(JSON.stringify({ output_base64: null }));
+  }
+}
